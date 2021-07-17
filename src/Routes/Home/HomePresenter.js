f4355c12089d15f6fe41d1dd17f01d1286fabd06
Jinsu Kim <wlstn8738@naver.com>
2021-07-17 23:30:34 +0900

    Chore: Modify home component

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 const Section = styled.div`
   text-align: center;
 `;
+
 const SectionBox = styled.div`
   display: inline-block;
   width: 700px;
@@ -20,17 +21,25 @@ const SectionBox = styled.div`
   text-align: center;
   z-index: -1;
 `;
-const Title = styled.h1`
-  font-size: 40px;
-  margin: 30px;
-  margin-bottom: 0;
-`;
+
 const MiddleBox = styled.div`
   width: 700px;
   height: 500px;
   display: table-cell;
   vertical-align: middle;
   text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 40px;
+  margin: 40px 30px 30px 30px;
+  margin-bottom: 0;
+`;
+
+const Content = styled.h2`
+  margin: 50px 20px 10px 20px;
+  font-size: 22px;
+  text-align: left;
 `;
 
 const Button = styled.button`
@@ -46,12 +55,6 @@ const Button = styled.button`
   cursor: pointer;
 `;
 
-const Content = styled.h2`
-  margin: 20px;
-  font-size: 25px;
-  text-align: left;
-`;
-
 const HomePresenter = () => (
   <>
     <Helmet>
@@ -63,16 +66,18 @@ const HomePresenter = () => (
         <Title>이용 가이드</Title>
         <MiddleBox>
           <div>
-            <Content>1. 편한 자세로 의자에 앉아 주세요</Content>
+            <Content>1. 어깨를 피고 편안한 자세로 의자에 앉아 주세요</Content>
             <Content>
-              2. 머리, 목, 어깨가 보이도록 측면 사진을 찍어 주세요
-            </Content>
-            <Content>3. 사진을 넣고 검사 시작하기를 눌러주세요</Content>
-            <Content>
-              4. 사진이 준비 되었으면 아래 시작하기 버튼을 눌러주세요
+              2. 머리, 목, 어깨가 보이도록 상체 측면 사진을 찍어 주세요
             </Content>
             <Content>
-              5. 상세한 내용은 좌측 메뉴의 이용가이드를 확인해 주세요
+              3. 사진을 넣고 검사하기 버튼을 누르면 측정 후 결과가 나옵니다
+            </Content>
+            <Content>
+              4. 자세한 이용가이드는 촤측 메뉴의 이용가이드를 확인해 주세요
+            </Content>
+            <Content>
+              5. 사진이 준비 되었으면 아래 시작하기 버튼을 눌러 주세요
             </Content>
             <br />
             <br />
@@ -80,7 +85,7 @@ const HomePresenter = () => (
           </div>
         </MiddleBox>
         <Link to="Detail" style={{ textDecoration: "none" }}>
-          <Button>검사 시작하기</Button>
+          <Button>시작하기</Button>
         </Link>
       </SectionBox>
     </Section>
